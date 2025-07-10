1bab58e9a687c129fde6e44218af8b081317909b
khanIrfan <khanirfan895765@gmail.com>
2025-07-10 13:57:44 +0530

    minor changes

--- backend/routes/auth.py
+++ backend/routes/auth.py
@@ -1,11 +1,14 @@
 from fastapi import APIRouter,HTTPException,status
 from schemas.auth import LoginData
+from dotenv import load_dotenv
+import os
+load_dotenv()
 
 router=APIRouter()
 
 @router.post("/login",status_code=status.HTTP_200_OK)
 def home(data:LoginData):
-    if data.email=="admin@gmail.com" and data.password=="admin":
+    if data.email==os.getenv("ADMIN_EMAIL") and data.password==os.getenv("ADMIN_PASSWORD"):
         return {"message":"sucessfull login"}
     else:
         raise HTTPException(detail="inavlid credentiasl",status_code=status.HTTP_401_UNAUTHORIZED)
